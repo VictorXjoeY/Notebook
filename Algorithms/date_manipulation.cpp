9fe2d2e0e0cdefb51e9437d0faf2d7e6cd3f56b1
VictorXjoeY <victor.forbes@hotmail.com>
2018-01-23 22:38:10 -0200

    Updating date manipulation stuff

---
+++ Algorithms/date_manipulation.cpp
@@ -0,0 +1,28 @@
+string week_day_str[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+string month_str[13] = {"", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+map<string, int> week_day_int = {{"Sunday", 0}, {"Monday", 1}, {"Tuesday", 2}, {"Wednesday", 3}, {"Thursday", 4}, {"Friday", 5}, {"Saturday", 6}};
+map<string, int> month_int = {{"January", 1}, {"February", 2}, {"March", 3}, {"April", 4}, {"May", 5}, {"June", 6}, {"July", 7}, {"August", 8}, {"September", 9}, {"October", 10}, {"November", 11}, {"December", 12}};
+int month[2][13] = {{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+
+// O(1) Checks if year y is a leap year.
+bool leap_year(int y){
+	return (y % 4 == 0 and y % 100 != 0) or y % 400 == 0;
+}
+
+// O(1) Increases the day by one.
+void update(int &d, int &m, int &y){
+	if (d == month[leap_year(y)][m]){
+		d = 1;
+
+		if (m == 12){
+			m = 1;
+			y++;
+		}
+		else{
+			m++;
+		}
+	}
+	else{
+		d++;
+	}
+}
