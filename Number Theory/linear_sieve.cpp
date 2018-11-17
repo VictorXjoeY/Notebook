d0f815da4efc76d5abf7ebfa6db84e0587995506
VictorXjoeY <victor.forbes@hotmail.com>
2018-11-16 23:36:54 -0200

    Linear Sieve

---
+++ Number Theory/linear_sieve.cpp
@@ -0,0 +1,21 @@
+#define N 1000000
+
+int mp[N + 1]; // mp[i] stores the minimum prime number which divides i. i is prime if it's not 0 and mp[i] == i.
+vector<int> p; // Stores prime numbers.
+
+/* O(N). */
+void sieve(){
+	int i, j;
+
+	for (i = 2; i <= N; i++){
+		if (!mp[i]){ // i is prime if we haven't found any smaller factors of it.
+			mp[i] = i;
+			p.push_back(i);
+		}
+
+		// Since every number x can be written as x = i * mp[x] we can iterate over every prime mp[x] (prime[j]).
+		for (j = 0; j < (int)p.size() and p[j] <= mp[i] and i * p[j] <= N; j++){
+			mp[i * p[j]] = p[j];
+		}
+	}
+}
--- Number Theory/sieve.cpp
+++ Number Theory/sieve.cpp
@@ -12,20 +12,16 @@ void sieve(){
 	prime[0] = prime[1] = false;
 
 	// For every number i up to sqrt(N). */
-	for (i = 2; i * i <= N; i++){
+	for (i = 2; i <= N; i++){
 		// If current number is prime.
 		if (prime[i]){
+			// Pushing the primes into an array.
+			p.push_back(i);
+
 			// Marking its multiples to not prime.
 			for (j = i * i; j <= N; j += i){
 				prime[j] = false;
 			}
-		}
-	}
-
-	// Pushing all the primes into an array.
-	for (i = 2; i <= N; i++){
-		if (prime[i]){
-			p.push_back(i);
 		}
 	}
 }
