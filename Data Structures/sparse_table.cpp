1db2d9df1dec5c7bcb5ae66011d61e4520254163
VictorXjoeY <victor.forbes@hotmail.com>
2018-11-20 17:52:34 -0200

    Sparse Table

---
+++ Data Structures/sparse_table.cpp
@@ -0,0 +1,39 @@
+#define N 100000
+#define L 20
+
+int table[N + 1][L + 1];
+int a[N + 1];
+int n;
+
+/* O(1) - Idempotent operation. */
+int merge(int x, int y){
+	return max(x, y);
+}
+
+/* O(N * Log(N)). */
+void build(){
+	int i, j;
+
+	// Base.
+	for (i = 1; i <= n; i++){
+		table[i][0] = a[i];
+	}
+
+	// Building for each 2^j <= n
+	for (j = 1; (1 << j) <= n; j++){
+		// Building for each [i, i + 2^j - 1]
+		for (i = 1; i + (1 << j) - 1 <= n; i++){
+			table[i][j] = merge(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
+		}
+	}
+}
+
+/* O(1). */
+int query(int l, int r){
+	int k;
+
+	// Finding greatest k such that 2^k <= r - l + 1
+	k = 32 - __builtin_clz(r - l + 1) - 1;
+
+	return merge(table[l][k], table[r - ((1 << k) - 1)][k]);
+}
