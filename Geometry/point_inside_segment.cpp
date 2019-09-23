690cd742821bb9c5d970451237905180f2c389de
Victor Forbes <victor.forbes@hotmail.com>
2019-09-23 14:29:48 +0100

    Point Inside Segment

---
+++ Geometry/point_inside_segment.cpp
@@ -0,0 +1,30 @@
+struct Point{
+	long long x, y;
+
+	Point(){}
+
+	Point(long long x, long long y){
+		this->x = x;
+		this->y = y;
+	}
+
+	// Vector subtraction.
+	Point operator - (const Point &b) const{
+		return Point(this->x - b.x, this->y - b.y);
+	}
+
+	// Cross product.
+	long long operator ^ (const Point &b) const{
+		return this->x * b.y - this->y * b.x;
+	}
+
+	// Dot product.
+	long long operator * (const Point &b) const{
+		return this->x * b.x + this->y * b.y;
+	}
+};
+
+/* O(1) - Returns true if the point Q is inside the segment AB. */
+bool point_inside_segment(const Point &q, const Point &a, const Point &b){
+	return ((b - a) ^ (q - a)) == 0 and ((b - a) * (q - a)) >= 0 and ((a - b) * (q - b)) >= 0;
+}
