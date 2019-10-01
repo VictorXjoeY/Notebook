32a22c9d720296c955841afd54b04448d8644dc8
Victor Forbes <victor.forbes@hotmail.com>
2019-10-01 04:46:04 +0100

    Shoelace interface update

--- Geometry/shoelace.cpp
+++ Geometry/shoelace.cpp
@@ -1,5 +1,3 @@
-#define N 100000
-
 struct Point{
 	long long x, y;
 
@@ -11,20 +9,17 @@ struct Point{
 	}
 };
 
-Point p[N + 1]; // (Input)
-int n; // (Input)
-
 /* O(N) - Computes the area of a simple (convex or concave) polygon. */
-double shoelace(){
+double shoelace(const vector<Point> &p){
 	long long ans;
 
 	ans = 0;
 
-	for (int i = 1; i < n; i++){
+	for (int i = 0; i < (int)p.size() - 1; i++){
 		ans += p[i].x * p[i + 1].y - p[i + 1].x * p[i].y;
 	}
 
-	ans += p[n].x * p[1].y - p[1].x * p[n].y;
+	ans += p.back().x * p[0].y - p[0].x * p.back().y;
 
 	return abs(ans) / 2.0;
 }
