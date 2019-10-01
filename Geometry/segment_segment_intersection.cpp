7982d49c124b29b690717d00a119a8a529d16d0c
Victor Forbes <victor.forbes@hotmail.com>
2019-10-01 04:44:06 +0100

    Segment Segment Intersection Check

---
+++ Geometry/segment_segment_intersection.cpp
@@ -0,0 +1,39 @@
+struct Point{
+	long long x, y;
+
+	Point(){}
+
+	Point(long long x_, long long y_){
+		this->x = x_;
+		this->y = y_;
+	}
+
+	// Vector subtraction.
+	Point operator - (const Point &b) const{
+		return Point(this->x - b.x, this->y - b.y);
+	}
+
+	// Cross product.
+	long long operator ^ (const Point &b) const{
+		return this->x * b.y - this->y * b.x;
+	}
+};
+
+/* O(1) - Extracts the sign of x. */
+int sign(long long x){
+	if (x < 0){
+		return -1;
+	}
+
+	if (x > 0){
+		return 1;
+	}
+
+	return 0;
+}
+
+/* O(1) - Returns true if the segment AB has any points in common with the segment CD. */
+bool segment_segment_intersects(const Point &a, const Point &b, const Point &c, const Point &d){
+	// If C is to the opposite side of D relative to the line AB and A is to the opposite side of B relative to line CD.
+	return sign((b - a) ^ (c - a)) * sign((b - a) ^ (d - a)) <= 0 and sign((d - c) ^ (a - c)) * sign((d - c) ^ (b - c)) <= 0;
+}
