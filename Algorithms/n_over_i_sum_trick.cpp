8bd2a35a5d7a2bf4fd618015428068205120be51
Victor Forbes <victor.forbes@hotmail.com>
2019-10-15 23:11:10 -0300

    n over i sum trick

---
+++ Algorithms/n_over_i_sum_trick.cpp
@@ -0,0 +1,16 @@
+long long f(long long x){
+	// Define this function.
+}
+
+/* O(sqrt(N)) - Calculates sum(floor(n / i)) for i from 1 to n. */
+long long trick(long long n){
+	long long ans, l, r;
+
+	ans = 0;
+
+	// Iterating over every range [l, r] that has the same value of floor(n / x) for l <= x <= r.
+	for (l = 1; l <= n; l = r + 1){
+		r = n / (n / l);
+		ans += (r - l + 1) * f(n / l);
+	}
+}
