22e5b341435373a4cb3e8ecd23a2efa2de662242
Victor Forbes <victor.forbes@hotmail.com>
2019-10-19 09:11:07 -0300

    Adding comments in Template about regex_replace and number of nodes in a segment tree

--- template.cpp
+++ template.cpp
@@ -7,6 +7,10 @@ using namespace std;
 
 // Be careful when solving an inequation such as x >= a / b. To get ceil(a / b) use the code floor_ceil.cpp under "Number Theory" folder.
 
+// Segment trees uses exactly 2 * N - 1 nodes, but we need to declare 2 * 2^(ceil(log2(N))) - 1 nodes if we use 2 * x and 2 * x + 1 as children nodes.
+
+// Use: s = regex_replace(s, regex(a), b); to replace every occurence of a for b in s.
+
 // How to generate random numbers and random permutations:
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 mt19937_64 rng_64(chrono::steady_clock::now().time_since_epoch().count());
