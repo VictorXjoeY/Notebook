6fdacd5563adfa50d9faebddee7afdaaf1cf1276
Victor Forbes <victor.forbes@hotmail.com>
2019-10-29 18:53:23 -0300

    Fixed Persistent Dynamic Segment Tree (still untested)

--- Data Structures/persistent_dynamic_segment_tree.cpp
+++ Data Structures/persistent_dynamic_segment_tree.cpp
@@ -37,24 +37,26 @@ int update(int cur, int l, int r, long long prop, int i, int j, long long x) {
 	}
 
 	ncur = ++last; // New node.
+	lchild[ncur] = lchild[cur];
+	rchild[ncur] = rchild[cur];
 
-	if (l >= i and r <= j) { // Fully in range.
+	if (l >= i and r <= j) { // Fully in range. "Leaf" of current update.
 		lazy[ncur] = prop + x;
-		return ncur;
+	}
+	else { // Partially in range.
+		// Updating children and linking the tree. Propagate downwards a value only if cur is a leaf.
+		lchild[ncur] = update(lchild[cur], l, m, prop + lazy[cur], i, j, x);
+		rchild[ncur] = update(rchild[cur], m + 1, r, prop + lazy[cur], i, j, x);
 	}
 
-	// Updating children and linking the tree. Propagate downwards a value only if cur is a leaf.
-	lchild[ncur] = update(lchild[cur], l, m, prop + lazy[cur], i, j, x);
-	rchild[ncur] = update(rchild[cur], m + 1, r, prop + lazy[cur], i, j, x);
-
-	// Merging children's results.
+	// Merging children's results, even if the current node was fully in range.
 	seg[ncur] = merge(value(lchild[ncur], l, m), value(rchild[ncur], m + 1, r));
 
 	return ncur;
 }
 
-/* O(Log(MAX)) - Use query(root[k], MIN, MAX, i, j) to query after the k'th update */
-long long query(int cur, int l, int r, int i, int j) {
+/* O(Log(MAX)) - Use query(root[k], MIN, MAX, 0, i, j) to query after the k'th update */
+long long query(int cur, int l, int r, long long prop, int i, int j) {
 	int m = (l + r) / 2;
 	long long nl, nr;
 
@@ -63,12 +65,12 @@ long long query(int cur, int l, int r, int i, int j) {
 	}
 
 	if ((l >= i and r <= j) or (!lchild[cur] and !rchild[cur])) { // Fully in range or leaf.
-		return value(cur, max(l, i), min(r, j));
+		return value(cur, max(l, i), min(r, j)) + (min(r, j) - max(l, i) + 1) * prop;
 	}
 
 	// Has both children, querying downwards.
-	nl = query(lchild[cur], l, m, i, j);
-	nr = query(rchild[cur], m + 1, r, i, j);
+	nl = query(lchild[cur], prop + lazy[cur], l, m, i, j);
+	nr = query(rchild[cur], prop + lazy[cur], m + 1, r, i, j);
 
 	return merge(nl, nr);
 }
