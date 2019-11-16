498d188e3bc7a0aec923261ff1bfe3113ec80b91
Victor Forbes <victor.forbes@hotmail.com>
2019-11-16 03:23:39 -0300

    Implemented Dinic's Algorithm

---
+++ Graph Theory/max_flow_min_cut_dinic.cpp
@@ -0,0 +1,203 @@
+constexpr int INF = 0x3f3f3f3f; // Has to be greater or equal to maximum flow.
+constexpr int N = 500;
+constexpr int FNN = N;
+
+vector<pair<int, int>> g[FNN + 1]; // (Input) Original graph, g[u] stores pairs (v, capacity).
+vector<pair<int, int>> r[FNN + 1]; // Residual graph, r[u] stores pairs (v, current capacity).
+vector<int> flow[FNN + 1]; // Flow graph, f[u][i] stores the flow going through the edge (u, r[u][i]).
+vector<int> rev[FNN + 1]; // Reverse edge mapping, rev[u][i] stores the index j such that u == r[r[u][i].first][j].first.
+int level[FNN + 1]; // level[u] stores the distance from the source to u in the residual graph.
+int p[FNN + 1]; // p[u] stores the index of the next relevant edge (u, r[u][p[u]].first) for the blocking flow DFS. Multiple pointers technique.
+bool seen[FNN + 1];
+int fnn; // (Input) Number of vertices in the flow network.
+
+/* O(V + E) - Builds the level graph. */
+bool bfs(int s, int t) {
+	queue<int> q;
+
+	// Clearing.
+	memset(seen, false, sizeof(seen));
+
+	// Initializing.
+	level[s] = 0;
+	seen[s] = true;
+	q.push(s);
+
+	while (!q.empty()) {
+		int u = q.front();
+		q.pop();
+
+		// For every edge (u, v).
+		for (int i = 0; i < r[u].size(); i++) {
+			int v = r[u][i].first;
+			int c = r[u][i].second; // Current capacity.
+
+			// If it was not visited and we can push a positive amount of flow.
+			if (!seen[v] and c > 0) {
+				// Updating v.
+				level[v] = level[u] + 1;
+				seen[v] = true;
+
+				if (v == t) { // Found a path from the source to the sink.
+					// We can already return because we won't use any other vertices from this same level other than the sink.
+					return true;
+				}
+
+				// Pushing v into the queue.
+				q.push(v);
+			}
+		}
+	}
+
+	return false;
+}
+
+/* O(V + E) - Pushes the blocking flow using V pointers. */
+int dfs(int u, int t, int f) {
+	if (u == t) { // Found sink, pushed f.
+		return f;
+	}
+
+	// For each edge (u, v).
+	for (; p[u] < r[u].size(); p[u]++) {
+		int v = r[u][p[u]].first;
+		int c = r[u][p[u]].second;
+		int pf;
+
+		// If v was seen by the BFS, is in the next layer, the residual capacity is positive and pushed a positive amount of flow.
+		if (seen[v] and level[v] == level[u] + 1 and c > 0 and (pf = dfs(v, t, min(f, c)))) {
+			int pv = rev[u][p[u]]; // Reverse edge index.
+
+			// Updating capacities in the residual graph.
+			r[u][p[u]].second -= pf;
+			r[v][pv].second += pf;
+
+			// Updating flow graph.
+			flow[u][p[u]] += pf;
+			flow[v][pv] -= pf;
+
+			// Returning the flow that was pushed. Next call to the DFS will go straight back to this edge, skipping saturated ones.
+			return pf;
+		}
+	}
+
+	// Didn't push any flow through u.
+	return 0;
+}
+
+/* O(min(F, V * V) * E) such that F is the maximum flow. 
+   O(min(V^(2 / 3), sqrt(E)) * E) for networks in which every edge has capacity 1.
+   O(sqrt(V) * E) for unit networks, networks in which every edge has an integer capacity and, apart from the sink and the source,
+   every vertex has either a single incoming edge of capacity one or a single outgoing edge of capacity one. */
+int dinic(int s, int t) {
+	int ans, f;
+
+	// Clearing.
+	for (int u = 1; u <= fnn; u++) {
+		r[u].clear();
+	}
+
+	// O(V + E) - Creating residual graph.
+	for (int u = 1; u <= fnn; u++) {
+		for (int i = 0; i < g[u].size(); i++) {
+			int v = g[u][i].first;
+			int c = g[u][i].second;
+
+			if (u != v) { // Ignoring self-loops.
+				r[u].push_back({v, c});
+				r[v].push_back({u, 0});
+			}
+		}
+	}
+
+	// O(E * Log(E)) - Merging parallel edges in the residual graph.
+	for (int u = 1; u <= fnn; u++) {
+		// Sorting so that parallel edges stay together.
+		vector<pair<int, int>> adj = r[u];
+		sort(adj.begin(), adj.end());
+		r[u].clear();
+
+		// Merging parallel edges.
+		for (int i = 0; i < adj.size(); i++) {
+			int v = adj[i].first;
+			int c = adj[i].second;
+
+			if (!r[u].empty() and v == r[u].back().first) {
+				r[u].back().second += c;
+			}
+			else { // Pushing new edge.
+				r[u].push_back({v, c});
+			}
+		}
+	}
+
+	// O(E * Log(E)) - Initializing flow and creating reverse edges references.
+	for (int u = 1; u <= fnn; u++) {
+		flow[u].assign(r[u].size(), 0);
+		rev[u].resize(r[u].size());
+
+		// For each edge (u, v).
+		for (int i = 0; i < r[u].size(); i++) {
+			int v = r[u][i].first;
+
+			// Retrieving u's position in v's adjacency list.
+			rev[u][i] = lower_bound(r[v].begin(), r[v].end(), make_pair(u, 0)) - r[v].begin();
+		}
+	}
+
+	ans = 0;
+
+	// While I can improve the flow.
+	while (bfs(s, t)) {
+		// Clearing pointers.
+		memset(p, 0, sizeof(p));
+
+		// Pushing blocking flow.
+		while ((f = dfs(s, t, INF))) {
+			ans += f;
+		}
+	}
+
+	return ans;
+}
+
+/* O(V + E). */
+void dfs(int u) {
+	seen[u] = true;
+
+	// For each edge (u, v).
+	for (int i = 0; i < r[u].size(); i++) {
+		int v = r[u][i].first;
+		int c = r[u][i].second;
+
+		// If v was not seen and the residual capacity of (u, v) is positive.
+		if (!seen[v] and c > 0) {
+			dfs(v);
+		}
+	}
+}
+
+/* O(min(F, V * V) * (V + E)) such that F is the maximum min-cut cost. */
+vector<pair<int, int>> min_cut(int s, int t) {
+	vector<pair<int, int>> ans;
+
+	// Running Edmonds-Karp to find max flow.
+	dinic(s, t);
+
+	// Running a dfs from the source in the residual graph.
+	memset(seen, false, sizeof(seen));
+	dfs(s);
+
+	for (int u = 1; u <= fnn; u++) {
+		for (int i = 0; i < r[u].size(); i++) {
+			int v = r[u][i].first;
+
+			// Edge (u, v) belongs to the min-cut if u is reachable from the source but v is not.
+			if (seen[u] and !seen[v]) {
+				ans.push_back({u, v});
+			}
+		}
+	}
+
+	return ans;
+}
