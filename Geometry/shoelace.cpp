10b02d38f05e1c9131b8a56246047649138c0935
Victor Forbes <victor.forbes@hotmail.com>
2020-01-02 23:03:02 +0100

    Updated comment on Shoelace.

--- Geometry/shoelace.cpp
+++ Geometry/shoelace.cpp
@@ -2,11 +2,10 @@ struct Point {
 	long long x, y;
 };
 
-/* O(N) - Computes the area of a simple (convex or concave) polygon. */
+/* O(N) - Computes the area of a convex or non-convex polygon, being it simple or self-intersecting.
+   Expects a polygon either in clockwise or in counter-clockwise order. */
 double shoelace(const vector<Point> &p) {
-	long long ans;
-
-	ans = 0;
+	long long ans = 0;
 
 	for (int i = 0; i < p.size() - 1; i++) {
 		ans += p[i].x * p[i + 1].y - p[i + 1].x * p[i].y;
