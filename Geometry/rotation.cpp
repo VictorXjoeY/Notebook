ab7bb662e1cd2238da08c6946048d110b978f12f
Victor Forbes <victor.forbes@hotmail.com>
2020-01-05 12:09:59 -0300

    Polygon rotation

--- Geometry/rotation.cpp
+++ Geometry/rotation.cpp
@@ -1,23 +1,39 @@
+// T must be a floating point type for the rotations to work as intended.
+template <class T>
 struct Point {
-	double x, y;
+	T x, y;
 
 	/* O(1) - Vector sum. */
-	Point operator + (const Point &b) const {
+	Point<T> operator + (const Point<T> &b) const {
 		return {this->x + b.x, this->y + b.y};
 	}
 
 	/* O(1) - Vector subtraction. */
-	Point operator - (const Point &b) const {
+	Point<T> operator - (const Point<T> &b) const {
 		return {this->x - b.x, this->y - b.y};
 	}
 };
 
 /* O(1) - Rotates vector p by theta radians. */
-Point rotate(const Point &p, double theta) {
+template <class T>
+Point<T> rotate(const Point<T> &p, double theta) {
 	return {p.x * cos(theta) - p.y * sin(theta), p.x * sin(theta) + p.y * cos(theta)};
 }
 
 /* O(1) - Rotates points p around pivot by theta radians. */
-Point rotate(const Point &pivot, const Point &p, double theta) {
+template <class T>
+Point<T> rotate(const Point<T> &pivot, const Point<T> &p, double theta) {
 	return pivot + rotate(p - pivot, theta);
+}
+
+/* O(N) - Rotates a polygon p around pivot by theta radians. */
+template <class T>
+vector<Point<T>> rotate(const Point<T> &pivot, const vector<Point<T>> &p, double theta) {
+	vector<Point<T>> ans(p.size());
+
+	for (int i = 0; i < p.size(); i++) {
+		ans[i] = rotate(pivot, p[i], theta);
+	}
+
+	return ans;
 }
