4c6b9de428aa28b7926d08b332efba1fb30faffa
Victor Forbes <victor.forbes@hotmail.com>
2020-02-10 13:53:03 +0000

    Comments on template about frequency and 2-sat

--- template.cpp
+++ template.cpp
@@ -16,6 +16,13 @@ using namespace std;
 // Trie is the most beautiful Data Structure. It is a more generic version of the Dynamic (Implicit) Segment Tree, since it doesn't need to be binary.
 // The Trie can behave just like a Segment Tree if you insert the value a[i] at the node that represents position i (following the path given by the decomposition of i in binary).
 
+// Frequency queries that come after all the updates can be done by inserting everything in a vector and doing upper_bound(x) - lower_bound(x)
+// for O(Log(N)) but with a smaller constant than using a map. This method is specially useful if you are working with the frequency of elements
+// of type double because you can adjust your precision by doing upper_bound(x + eps) - lower_bound(x - eps).
+
+// 2-SAT problems where each term is XOR instead of OR can be solved by building a graph where each variable is a vertex and each term is an edge
+// and then "bicoloring" according to the edge restrictions.
+
 // Use: s = regex_replace(s, regex(a), b); to replace every occurence of a for b in s.
 
 // To check if a * b is going to overflow you just need to check if a > numeric_limits<long long>::max() / b (assuming that a and b are positive).
