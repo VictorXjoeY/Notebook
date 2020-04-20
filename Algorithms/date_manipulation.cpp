d212ab576e485aea52b6789ec2e8568272ed750e
Victor Forbes <victor.forbes@hotmail.com>
2020-04-20 05:52:04 +0200

    Updating Week Days to start from Monday

--- Algorithms/date_manipulation.cpp
+++ Algorithms/date_manipulation.cpp
@@ -1,6 +1,6 @@
-string week_day_str[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+string week_day_str[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 string month_str[13] = {"", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-map<string, int> week_day_int = {{"Sunday", 0}, {"Monday", 1}, {"Tuesday", 2}, {"Wednesday", 3}, {"Thursday", 4}, {"Friday", 5}, {"Saturday", 6}};
+map<string, int> week_day_int = {{"Monday", 0}, {"Tuesday", 1}, {"Wednesday", 2}, {"Thursday", 3}, {"Friday", 4}, {"Saturday", 5}, {"Sunday", 6}};
 map<string, int> month_int = {{"January", 1}, {"February", 2}, {"March", 3}, {"April", 4}, {"May", 5}, {"June", 6}, {"July", 7}, {"August", 8}, {"September", 9}, {"October", 10}, {"November", 11}, {"December", 12}};
 int month[2][13] = {{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
 
--- Algorithms/zeller.cpp
+++ Algorithms/zeller.cpp
@@ -1,9 +1,9 @@
-/* O(1) - Given the date, returns the day of the week from 0 to 6 starting from Sunday. */
+/* O(1) - Given the date, returns the day of the week from 0 to 6 starting from Monday. */
 int zeller(long long d, long long m, long long y) {
 	if (m == 1 or m == 2) {
 		m += 12;
 		y--;
 	}
 
-	return (((d + ((13 * (m + 1)) / 5) + y + (y / 4) - (y / 100) + (y / 400)) % 7) + 6) % 7;
+	return (((d + ((13 * (m + 1)) / 5) + y + (y / 4) - (y / 100) + (y / 400)) % 7) + 5) % 7;
 }
